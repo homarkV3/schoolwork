0c46659274d4bb5ff00ae07545daa19e81cc0c01
homarkV3 <89234598+homarkV3@users.noreply.github.com>
2023-02-23 23:03:53 -0700

    update
    update

--- GitHub/schoolwork/js/discord bot/index.js
+++ GitHub/schoolwork/js/discord bot/index.js
@@ -1,9 +1,25 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits} = require('discord.js');
 const { token } = require('./config.json');
 const { LoremIpsum } = require('lorem-ipsum')
 const puppeteer = require("puppeteer")
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates, 
+        GatewayIntentBits.MessageContent
+    ] });
 const fs = require('fs')
+const nodemailer = require('nodemailer')
+const open =  require('open')
+const { DisTube } = require('distube');
+
+client.distube = new DisTube(client, {
+    leaveOnStop: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
+})
 
 var help = fs.readFileSync("./help.txt", 'utf8');
 // When the client is ready, run this code (only once)
@@ -12,52 +28,109 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('messageCreate', async Message => {
-	if (Message.author.bot) return
-    if (!Message.content.startsWith('!')) return
-    input = Message.content.split(" ")
+client.on('messageCreate', async message => {
+	if (message.author.bot) return
+    if (!message.content.startsWith('!')) return
+    input = message.content.split(" ")
+
     switch (input[0]) {
         case(`!hello`):
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
             console.log("world")
-            Message.reply("world")
+            message.reply("world")
             break
         case(`!help`):
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
             console.log(help)
-            Message.reply(help)
+            message.reply(help)
             break
         case(`!lorem`):
+            if (input.length > 2) return message.reply("please read the help and follow as this does not take more then 1 argument")
             const lorem = new LoremIpsum()
             console.log(input)
             if (input.length == 1) {
                 console.log(lorem.generateWords(10))
-                Message.reply(lorem.generateWords(10))
+                message.reply(lorem.generateWords(10))
             } else {
                 console.log(lorem.generateWords(Number(input[1])))
-                Message.reply(lorem.generateWords(Number(input[1])))
+                message.reply(lorem.generateWords(Number(input[1])))
             }
             break
         case(`!virus`):
-            await Message.reply(lorem.generateWords('```diff\n- YOU KILLED ME!\1- I\'LL SEE YOU IN HELL.```'))
-            currCases = getCovid()
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
+            getCovid()
             break
         case(`!dadjoke`):
-        
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
+            joke("./dadjoke.txt", 100)
             break
         case(`!chuck`):
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
+            joke("./chuck.txt", 101)
             break
         case(`!die`):
+            if (input.length > 1) return message.reply("please read the help and follow as this does not take any other argument")
             process.exit(0)
-        case(`!text`):
-            break
         case(`!email`):
+            if (input.length > 4) return message.reply("please read the help and follow as this does not take more then 3 argument")
+            sendemail(input[1], input[2], input[3])
             break
-        case(`!music`):
+        case(`!spotify`):
+            if (input.length > 2) return message.reply("please read the help and follow as this does not take more then 1 argument")
+            message.reply("opening spotify on web")
+            open(`https://open.spotify.com/search/${input[1]}`);
             break
-        case(`!weather`):
+        case(`!play`):
+            const channel = message.member.voice.channel
+            if(!channel) return message.channel.send('please join a channel for this to work')
+            console.log(client.distube.voices.get(message.guild))
+            await message.reply("**Searching and attempting...**")
+            await message.reply("Searching done :ok_hand: ")
+            await client.distube.play(channel, input.join(' '),{
+                textChannel: message.channel,
+                member: message.member,
+            })
             break
         default:
-            Message.reply(`Unknown command ${msg.content}. If you type it again, I'll kill you. Type !help to see safer options.`)
+            message.reply(`Unknown command ${message.content}. If you type it again, I'll kill you. Type !help to see safer options.`)
             break
+        }
+
+        function joke(file, amount){
+            var joke = fs.readFileSync(file, 'utf8');
+            jokes = joke.split("\n")
+            console.log(`${jokes[getRandomInt(amount)]}`)
+            message.reply(`${jokes[getRandomInt(amount)]}`)
+        }
+
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+
+        function sendemail(to, subj, content) {
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: `homaxx98@gmail.com`,
+                  pass: `gxjdsqgdrmmlqzux`
+                }
+              })
+              var mailOptions = {
+                from: `homaxx98@gmail.com`,
+                to: `${to}`,
+                subject: `${subj}`,
+                text: `${content}`
+              }
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  message.reply(`${error}`)
+                  console.log(error)
+                } else {
+                  message.reply('Email sent: ' + info.response)
+                  console.log('Email sent: ' + info.response)
+                }
+              })
         }
 
         function getCovid() {
@@ -71,7 +144,7 @@ client.on('messageCreate', async Message => {
                     const rawData = await page.$("strong")
                     const text = await (await rawData.getProperty('textContent')).jsonValue()
                     console.log(`current covid cases in utah county: ${text}`)
-                    Message.reply(`current covid cases in utah county: ${text}`)
+                    message.reply(`current covid cases in utah county: ${text}`)
                 } catch (error) {
                     console.log('error', error)
                 }
@@ -79,6 +152,5 @@ client.on('messageCreate', async Message => {
             })()
         }
 });
-
 // Log in to Discord with your client's token
 client.login(token);
