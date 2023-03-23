47b330932c4d5190ddc078d7fbcb279bfd3c12ff
homarkV3 <89234598+homarkV3@users.noreply.github.com>
2023-03-23 10:51:52 -0600

    update
    update

--- GitHub/schoolwork/networks2/project1/WSEchoClientv6.c
+++ GitHub/schoolwork/networks2/project1/WSEchoClientv6.c
@@ -1,8 +1,8 @@
 // CS 2690 Program 1 
 // Simple Windows Sockets Echo Client (IPv6)
 // Last update: 2/12/19
-// <Your name here> <Your section here> <Date>
-// <Your Windows version and Visual Studio version>
+// Hoyoung Kim cs 2690 3/5/2023
+// Mac OS 12.6.2, VS Code 
 //
 // Usage: WSEchoClientv6 <server IPv6 address> <server port> <"message to echo">
 // Companion server is WSEchoServerv6
@@ -22,7 +22,6 @@
 #include <stdio.h>       // for print functions
 #include <stdlib.h>      // for exit() 
 #include "winsock_wrapper.h"
-#include "DisplayFatalErr.c"
 
 // #include <winsock2.h>	 // for Winsock2 functions
 // #include <ws2tcpip.h>    // adds support for getaddrinfo & getnameinfo for v4+6 name resolution
@@ -54,7 +53,11 @@ void main(int argc, char *argv[])   // argc is # of strings following command, a
 	char *serverIPaddr = argv[1];
 	int serverPort = atoi(argv[2]);
 	char *msg = argv[3];
+	int mLength = 0;
 
+	while (msg[mLength] != '\0'){
+		mLength++;
+	}
 	// Initialize Winsock 2.0 DLL. Returns 0 if ok. If this fails, fprint error message to stderr as above & exit(1).  
 	if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0){
 		printf(stderr, "WSA startup Failed, so sad, your dad.\n");
@@ -64,11 +67,10 @@ void main(int argc, char *argv[])   // argc is # of strings following command, a
 	// we can signal any errors as shown on next line:
    	int sock = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
 	if (sock == INVALID_SOCKET) {
-		DisplayFatalErr("socket() function failed.");
+		DisplayFatalErr("socket() function failed.\n");
 	}
 	// Display helpful confirmation messages after key socket calls like this:
-	printf("Socket created successfully.  Press any key to continue...");
-	getchar();     // needed to hold console screen open
+	printf("Socket created successfully.\n");
 	
 	// If doing extra credit IPv4 address handling option, add the setsockopt() call as follows...
 	// if (perrno = setsockopt(sock, IPROTO_IPV6, IPV6_V6ONLY, (char *)&v6Only, sizeof(v6Only)) != 0)
@@ -76,26 +78,55 @@ void main(int argc, char *argv[])   // argc is # of strings following command, a
    	
 	// Zero out the sockaddr_in6 structure and load server info into it.  See slide 11-15.
 	// Don't forget any necessary format conversions.
+	memset(&serverInfo, 0, sizeof(serverInfo));
     serverInfo.sin6_family = AF_INET6;         // address family = IPv6
 	serverInfo.sin6_port = htons(serverPort);  // convert int port to ntwk order*
 	// Convert cmd line server addr from char to ntwk form, load into sockaddr_in6 
 	inet_pton(AF_INET6, serverIPaddr, &serverInfo.sin6_addr);
+	
+	
 	// Attempt connection to the server.  If it fails, call DisplayFatalErr() with appropriate message,
 	// otherwise printf() confirmation message
-    if (connect(sock, (struct sockaddr *) &serverInfo, sizeof(serverInfo)) < 0)
+    if (connect(sock, (struct sockaddr *) &serverInfo, sizeof(serverInfo)) < 0){
     	DisplayFatalErr("connect() function failed."); 
-	// Send message to server (without '\0' null terminator). Check for null msg (length=0) & verify all bytes were sent...
-	// ...else call DisplayFatalErr() with appropriate message as before
- 
- 	// Retrieve the message returned by server.  Be sure you've read the whole thing (could be multiple segments). 
-	// Manage receive buffer to prevent overflow with a big message.
- 	// Call DisplayFatalErr() if this fails.  (Lots can go wrong here, see slides.)
- 	
+	}
+	printf("connected successfully\n");
+	int msgLen = strlen(msg);
+	printf("%d \n",msgLen);
+	int offset = 0;
+	printf("%d \n",offset);
+	while (offset <= msgLen) {
+		//send() returns # bytes sent if successful
+		int send_result = send(sock, &msg[offset], msgLen - offset, 0);
+		if (send_result != 0){
+			offset += send_result; 
+		} else if (offset == msgLen){
+			break;
+		} else {
+			DisplayFatalErr("error message or send the rest with more send() cells");
+		}
+	}; 
+	//todo handle message greater than RCVBUFSIZ correctly from the commandline
+	//we handle the case where msglen > RCVBUFSIZ
+	char rcvBuffer[RCVBUFSIZ] = { 0 };
+	offset = 0;
+	int bytesRead = 0;
+	while (offset < msgLen) {
+		int bytesRead = recv(sock, rcvBuffer + offset, RCVBUFSIZ - 1 - offset, 0);
+		if (bytesRead <= 0){
+			DisplayFatalErr("recv failure");
+		} else {
+			offset += bytesRead;
+		}
+		printf(rcvBuffer);
  	// Display ALL of the received message, in printable C string format.
-    
- 	// Close the TCP connection (send a FIN) & print appropriate message.
+	};
 
+ 	// Close the TCP connection (send a FIN) & print appropriate message.
+	closesocket(sock);
+	printf("\nsocket closed.\n");
 	// Release the Winsock DLL
+	WSACleanup();
 	
 	exit(0);
 }
