4b874bdee268b2d2a7cfeeec43aa15c17fce3e90
homarkV3 <89234598+homarkV3@users.noreply.github.com>
2023-04-30 20:54:41 -0600

    up
    up

--- GitHub/schoolwork/os/assn6/assn5.cpp
+++ GitHub/schoolwork/os/assn6/assn5.cpp
@@ -161,22 +161,23 @@ int main() {
     while (std::cin >> start_time >> duration) {
         processes.push_back({process_id++, start_time, duration, -1, -1, -1, -1});
     }
+    double fcfs_resp, fcfs_ta, fcfs_wait, sjf_resp, sjf_ta, sjf_wait, srtf_resp, srtf_ta, srtf_wait, rr_resp, rr_ta, rr_wait;
+    std::cout << std::fixed << std::setprecision(2);
 
     std::vector<Process> fcfs_result = first_come_first_served(processes);
-    std::vector<Process> sjf_result = shortest_job_first(processes);
-    std::vector<Process> srtf_result = shortest_remaining_time_first(processes);
-    std::vector<Process> rr_result = round_robin(processes, 100);
-
-    double fcfs_resp, fcfs_ta, fcfs_wait, sjf_resp, sjf_ta, sjf_wait, srtf_resp, srtf_ta, srtf_wait, rr_resp, rr_ta, rr_wait;
     calculate_avg_times(fcfs_result, fcfs_resp, fcfs_ta, fcfs_wait);
-    calculate_avg_times(sjf_result, sjf_resp, sjf_ta, sjf_wait);
-    calculate_avg_times(srtf_result, srtf_resp, srtf_ta, srtf_wait);
-    calculate_avg_times(rr_result, rr_resp, rr_ta, rr_wait);
-
-    std::cout << std::fixed << std::setprecision(2);
     std::cout << "First Come, First Served\nAvg. Resp.:" << fcfs_resp << ", Avg. T.A.:" << fcfs_ta << ", Avg. Wait:" << fcfs_wait << std::endl;
+
+    std::vector<Process> sjf_result = shortest_job_first(processes);
+    calculate_avg_times(sjf_result, sjf_resp, sjf_ta, sjf_wait);
     std::cout << "Shortest Job First\nAvg. Resp.:" << sjf_resp << ", Avg. T.A.:" << sjf_ta << ", Avg. Wait:" << sjf_wait << std::endl;
+
+    std::vector<Process> srtf_result = shortest_remaining_time_first(processes);
+    calculate_avg_times(srtf_result, srtf_resp, srtf_ta, srtf_wait);
     std::cout << "Shortest Remaining Time First\nAvg. Resp.:" << srtf_resp << ", Avg. T.A.:" << srtf_ta << ", Avg. Wait:" << srtf_wait << std::endl;
+
+    std::vector<Process> rr_result = round_robin(processes, 100);
+    calculate_avg_times(rr_result, rr_resp, rr_ta, rr_wait);
     std::cout << "Round Robin with Time Quantum of 100\nAvg. Resp.:" << rr_resp << ", Avg. T.A.:" << rr_ta << ", Avg. Wait:" << rr_wait << std::endl;
 
     return 0;
